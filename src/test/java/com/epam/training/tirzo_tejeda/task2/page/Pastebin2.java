2d30130f13b62f3a45e29926a31729adce38f319
ttejeda02 <ttejeda.rguez@gmail.com>
2024-07-04 12:09:24 -0600

    solved: task 2

--- src/test/java/com/epam/training/tirzo_tejeda/task2/page/Pastebin2.java
+++ src/test/java/com/epam/training/tirzo_tejeda/task2/page/Pastebin2.java
@@ -1,11 +1,15 @@
 package com.epam.training.tirzo_tejeda.task2.page;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.time.Duration;
 
-public class Pastebin {
+public class Pastebin2 {
     private static final String PASTEBIN_URL = "https://pastebin.com/";
     WebDriver driver;
 
@@ -15,11 +19,45 @@ public class Pastebin {
     @FindBy (id = "select2-postform-format-container")
     WebElement syntaxSelector;
 
-    @FindBy (id = "")
+    @FindBy (id = "select2-postform-expiration-container")
+    WebElement expirationSelector;
 
-    public Pastebin openPage(WebDriver driver){
+    @FindBy (id = "postform-name")
+    WebElement nameInput;
+
+    @FindBy (xpath = "//button[contains(text(), \"Create New Paste\")]")
+    WebElement createButton;
+
+    public Pastebin2 openPage(WebDriver driver){
         this.driver = driver;
         driver.get(PASTEBIN_URL);
         PageFactory.initElements(driver, this);
+        new WebDriverWait(driver, Duration.ofSeconds(10))
+                .until(ExpectedConditions.visibilityOf(codeInput));
+
+        return this;
+    }
+
+    public Pastebin2 fillForm(String code, String title){
+        codeInput.sendKeys(code);
+
+        syntaxSelector.click();
+        driver.findElement(By.xpath("//li[contains(text(), \"Bash\")]")).click();
+        expirationSelector.click();
+        driver.findElement(By.xpath("//li[contains(text(), \"10 Minutes\")]")).click();
+
+        nameInput.sendKeys(title);
+
+        return this;
+    }
+
+    public String[] createNewPaste(){
+        createButton.click();
+        String[] info = new String[3];
+        info[0] = driver.getTitle();
+        info[1] = driver.findElement(By.xpath("//div[@class=\"left\"]/a[1]")).getText();
+        info[2] = driver.findElement(By.xpath("/html/body/div[1]/div[2]/div[1]/div[1]/div[4]/div[2]/ol/li[2]/div")).getText();
+
+        return info;
     }
 }
--- src/test/java/com/epam/training/tirzo_tejeda/task2/test/Pastebin2Test.java
+++ src/test/java/com/epam/training/tirzo_tejeda/task2/test/Pastebin2Test.java
@@ -1,4 +1,34 @@
 package com.epam.training.tirzo_tejeda.task2.test;
 
+import com.epam.training.tirzo_tejeda.task2.page.Pastebin2;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Pastebin2Test {
+    WebDriver driver;
+
+    @Before
+    public void browserSetup() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
+    @Test
+    public void pastebin2() {
+        final String TITLE = "how to gain dominance among developers";
+        final String CODE = "git config --global user.name  \"New Sheriff in Town\"\n" +
+                "git reset $(git commit-tree HEAD^{tree} -m \"Legacy code\")\n" +
+                "git push origin master --force";
+        String[] info = new Pastebin2()
+                .openPage(driver)
+                .fillForm(CODE, TITLE)
+                .createNewPaste();
+        driver.quit();
+        Assert.assertTrue(info[0].contains(TITLE));
+        Assert.assertEquals(info[1], "Bash");
+        Assert.assertEquals(info[2], "git reset $(git commit-tree HEAD^{tree} -m \"Legacy code\")");
+    }
 }
